351fe9d7fc012a5e73411ef45afd892246fa7dec
owengetzeng <owentzeng35@gmail.com>
2025-04-04 23:11:25 -0400

    Wall Jumps

--- DDM 2D Unity Game/Assets/PlayerMovement.cs
+++ DDM 2D Unity Game/Assets/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public Rigidbody2D rb;
+    bool isFacingRight = true;
     [Header("Movement")]
     public float moveSpeed = 5f;
     float horizontalMovement;
@@ -18,11 +19,27 @@ public class PlayerMovement : MonoBehaviour
     public Transform groundCheckPos;
     public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
     public LayerMask groundLayer;
+    bool isGrounded;
 
      [Header("Gravity")]
      public float baseGravity = 2f;
      public float maxFallSpeed = 18f;
      public float fallSpeedMultiplier = 2f;
+
+    [Header("WallCheck")]
+    public Transform wallCheckPos;
+    public Vector2 wallCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask wallLayer;
+
+    [Header("WallMovement")]
+    public float wallSlideSpeed = 2;
+    bool isWallSliding;
+
+    bool isWallJumping;
+    float wallJumpDirection;
+    float wallJumpTime = 0.5f;
+    float wallJumpTimer;
+    public Vector2 wallJumpPower = new Vector2(5f, 10f);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -32,9 +49,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
+        
         GroundCheck();
         Gravity();
+       
+        ProcessWallSlide();
+        ProcessWallJump();
+if(!isWallJumping)
+{
+ rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
+  Flip();
+}
+       
     }
 
     private void Gravity() {
@@ -47,6 +73,40 @@ public class PlayerMovement : MonoBehaviour
             rb.gravityScale = baseGravity;
         }
     }
+
+private void ProcessWallSlide()
+{
+    if (!isGrounded && WallCheck() && horizontalMovement != 0)
+    {
+        isWallSliding = true;
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, Mathf.Max(rb.linearVelocity.y, -wallSlideSpeed));
+    }
+    else
+    {
+        isWallSliding = false;
+    }
+}
+
+private void ProcessWallJump(){
+    if(isWallSliding)
+    {
+        isWallJumping = false;
+        wallJumpDirection = -transform.localScale.x;
+        wallJumpTimer = wallJumpTime;
+
+        CancelInvoke(nameof(CancelWallJump));
+    }
+    else if(wallJumpTimer > 0f)
+    {
+        wallJumpTimer -= Time.deltaTime;
+    }
+}
+
+private void CancelWallJump()
+{
+    isWallJumping = false;
+
+}
 
     public void Move(InputAction.CallbackContext context)
     {
@@ -67,8 +127,22 @@ public class PlayerMovement : MonoBehaviour
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
             jumpsRemaining--;
         }
+    }
+        if(context.performed && wallJumpTimer > 0f)
+        {
+            isWallJumping = true;
+            rb.linearVelocity = new Vector2(wallJumpDirection * wallJumpPower.x, wallJumpPower.y);
+            wallJumpTimer = 0;
+
+            if(transform.localScale.x != wallJumpDirection) {
+                  isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+            }
+
+            Invoke(nameof(CancelWallJump), wallJumpTime + 0.1f);
         }
-        
     }
 
     private void GroundCheck()
@@ -76,6 +150,27 @@ public class PlayerMovement : MonoBehaviour
         if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
         {
             jumpsRemaining = maxJumps;
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    private bool WallCheck()
+    {
+        return Physics2D.OverlapBox(wallCheckPos.position, wallCheckSize, 0, wallLayer);
+    }
+
+    public void Flip()
+    {
+        if(isFacingRight && horizontalMovement < 0 || !isFacingRight && horizontalMovement > 0)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
         }
     }
 
@@ -83,5 +178,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Gizmos.color = Color.white;
         Gizmos.DrawWireCube(groundCheckPos.position, groundCheckSize);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(wallCheckPos.position, wallCheckSize);
     }
 }
